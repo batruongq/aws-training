e7b92cf7a3ae57d9280c65ed22fdf640c4a448a8
Ba Thi Truong <ba.truongthi@asnet.com.vn>
2022-08-24 16:41:17 +0700

    Update code for functions

---
+++ aws-amplify-training-v1/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,38 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "awsamplifytrainingv1employeeapi": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "awsamplifytrainingv1userapi": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "awsamplifytrainingv1": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "auth": {
+        "awsamplifytrainingv1584a4683": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        },
+        "userPoolGroups": {
+            "awsamplifytrainingv1GroupRole": "string"
+        }
+    }
+}
---
+++ aws-amplify-training-v1/amplify/backend/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'function/awsamplifytrainingv1employeeapi/src/index': path.resolve(__dirname, 'src/function/awsamplifytrainingv1employeeapi/index.ts')
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.webpack')
+          ]
+        ],
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: [
+    /^aws-sdk/
+  ],
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
+  ],
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, ''),
+    filename: '[name].js'
+  },
+  optimization: {
+    minimize: false
+  }
+};
